ec3eda2785e4552704199c1f25d5547aceecea37
Marco Nicosia <mnicosia@pivotal.io>
2018-04-13 19:37:50 -0700

    Now performs the restore, also indenting fixes

--- restore.sh
+++ restore.sh
@@ -7,20 +7,20 @@
 # s3SecretKey
 
 if [ "XX" = "${VCAP_SERVICES}XX" ]; then
-   echo "Required environment variable: VCAP_SERVICES"
-   exit 1
+  echo "Required environment variable: VCAP_SERVICES"
+  exit 1
 fi
 if [ "XX" = "${s3Bucket}XX" ]; then
-   echo "Required environment variable: s3Bucket"
-   exit 1
+  echo "Required environment variable: s3Bucket"
+  exit 1
 fi
 if [ "XX" = "${s3AccessKey}XX" ]; then
-   echo "Required environment variable: s3AccessKey"
-   exit 1
+  echo "Required environment variable: s3AccessKey"
+  exit 1
 fi
 if [ "XX" = "${s3SecretKey}XX" ]; then
-   echo "Required environment variable: s3SecretKey"
-   exit 1
+  echo "Required environment variable: s3SecretKey"
+  exit 1
 fi
 
 P_MYSQL=$(echo $VCAP_SERVICES | jq -r '.["p-mysql"][]')
@@ -39,11 +39,11 @@ listRequest="GET\n\n${contentType}\n${dateValue}\n/${s3Bucket}/"
 signature1=`echo -en ${listRequest} | openssl sha1 -hmac ${s3SecretKey} -binary | base64`
 
 curl -o ${fileList} -s -i -X GET \
-          -H "Host: ${s3Bucket}.s3.amazonaws.com" \
-          -H "Content-Type: ${contentType}" \
-          -H "Date: ${dateValue}" \
-          -H "Authorization: AWS ${s3AccessKey}:${signature1}" \
-          https://${s3Bucket}.s3.amazonaws.com/
+  -H "Host: ${s3Bucket}.s3.amazonaws.com" \
+  -H "Content-Type: ${contentType}" \
+  -H "Date: ${dateValue}" \
+  -H "Authorization: AWS ${s3AccessKey}:${signature1}" \
+  https://${s3Bucket}.s3.amazonaws.com/
 
 ARTIFACT=$(cat ${fileList} |./find-files-xml.pl | sort | tail -1)
 
@@ -54,9 +54,9 @@ artifactRequest="GET\n\n${contentType}\n${dateValue}\n/${s3Bucket}/${ARTIFACT}"
 
 signature2=`echo -en ${artifactRequest} | openssl sha1 -hmac ${s3SecretKey} -binary | base64`
 
-curl -o /var/tmp/${ARTIFACT} -s -X GET \
-          -H "Host: ${s3Bucket}.s3.amazonaws.com" \
-          -H "Content-Type: ${contentType}" \
-          -H "Date: ${dateValue}" \
-          -H "Authorization: AWS ${s3AccessKey}:${signature2}" \
-          https://${s3Bucket}.s3.amazonaws.com/${ARTIFACT}
+curl -s -X GET \
+  -H "Host: ${s3Bucket}.s3.amazonaws.com" \
+  -H "Content-Type: ${contentType}" \
+  -H "Date: ${dateValue}" \
+  -H "Authorization: AWS ${s3AccessKey}:${signature2}" \
+  https://${s3Bucket}.s3.amazonaws.com/${ARTIFACT} | ./mysql -u${USERNAME} -p${PASSWORD} -h${HOSTNAME} ${DATABASE}
