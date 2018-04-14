0258e978a9f384b910b0d76509625b5b102d49e5
Marco Nicosia <mnicosia@pivotal.io>
2018-04-13 21:13:52 -0700

    Fix up bash flags, no debug mode.

--- dump.sh
+++ dump.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -u
 
 ## Relies on environment variables:
 # VCAP_SERVICES
--- restore.sh
+++ restore.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ux
+#!/bin/bash -u
 
 ## Relies on environment variables:
 # VCAP_SERVICES
